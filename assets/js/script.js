d2ef08ad27243b7901502efa17246bfcfb5c3aba
Shandilya Anjali <unnatis.encircle@gmail.com>
2025-04-30 19:26:18 +0530

    Dental lmplants

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,60 @@ search.addEventListener("click", function () {
 closes.addEventListener("click", function () {
   searchbar.style.display = "none";
 });
+
+let product = document.getElementById("product");
+let productmenu = document.getElementById("product-menu");
+let dropicon = document.getElementById("drop-icon");
+
+product.addEventListener("click", function () {
+  productmenu.style.display =
+    productmenu.style.display === "block" ? "none" : "block";
+  dropicon.style.rotate =
+    productmenu.style.display === "block" ? "180deg" : "0deg";
+});
+
+let toggle = document.getElementById("toggle");
+let navbar = document.getElementById("navbar");
+
+toggle.addEventListener("click", function () {
+  navbar.style.display = navbar.style.display === "block" ? "none" : "block";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const bannerSwiper = new Swiper(".banner-slide", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: false,
+      clickable: true,
+    },
+    spaceBetween: 30,
+  });
+
+  const swiper = new Swiper(".swiper-slider-3", {
+    centeredSlides: true,
+    slidesPerView: 1,
+    grabCursor: true,
+    freeMode: false,
+    loop: true,
+    mousewheel: false,
+    keyboard: {
+      enabled: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1.25,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+    },
+  });
+});
